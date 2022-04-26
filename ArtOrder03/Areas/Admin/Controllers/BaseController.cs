f70cb285bfc2bbabe9a7248ddd48f26ec6e33a7f
NikkiIV <prince_the_ripper@abv.bg>
2022-04-26 19:54:52 +0300

    Add Admin Area

---
+++ ArtOrder03/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using ArtOrder03.Core.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtOrder03.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstants.Roles.Administrator)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+    }
+}
--- ArtOrder03/Areas/Admin/Controllers/UserController.cs
+++ ArtOrder03/Areas/Admin/Controllers/UserController.cs
@@ -1,28 +1,28 @@
-﻿using ArtOrder03.Core.Constants;
-using ArtOrder03.Core.Contracts;
-using Microsoft.AspNetCore.Authorization;
+﻿using ArtOrder03.Core.Contracts;
+using ArtOrder03.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ArtOrder03.Controllers
+namespace ArtOrder03.Areas.Admin.Controllers
 {
     public class UserController : BaseController
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly IUserService service;
 
         public UserController(
             RoleManager<IdentityRole> _roleManager,
+            UserManager<ApplicationUser> _userManager,
             IUserService _service)
         {
             roleManager = _roleManager;
+            userManager = _userManager;
             service = _service;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
     }
 }
